f4177c6b8f4f98c7a6deb66781653c1e443d742a
Geeky Mon2 <geekymon2@gmail.com>
2021-06-28 21:31:01 +1000

    added eureka client to the car-listing-service

--- car-listing-service/src/main/java/com/geekymon2/carmarketplace/CarListingSvcApp.java
+++ car-listing-service/src/main/java/com/geekymon2/carmarketplace/CarListingSvcApp.java
@@ -2,7 +2,9 @@ package com.geekymon2.carmarketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CarListingSvcApp {
 
