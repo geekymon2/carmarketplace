c2ecaa637b3a3ac3fefe1c03ed382210a582fe7c
Geeky Mon2 <geekymon2@gmail.com>
2021-07-04 14:23:39 +1000

    checkpoint 1.2 - project structure setup

--- imagestore-service/src/main/java/com/geekymon2/carmarketplace/carimagestoreservice/ImageStoreServiceApplication.java
+++ imagestore-service/src/main/java/com/geekymon2/carmarketplace/carimagestoreservice/ImageStoreServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class CarImageStoreServiceApplication {
+public class ImageStoreServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarImageStoreServiceApplication.class, args);
+		SpringApplication.run(ImageStoreServiceApplication.class, args);
 	}
 
 }
