4597c071181dc0114c0193e425679a8edb36998a
Geeky Mon2 <geekymon2@gmail.com>
2021-07-10 17:17:36 +1000

    update

--- car-info-service/src/main/java/com/geekymon2/carmarketplace/carinfoservice/data/ModelsData.java
+++ car-info-service/src/main/java/com/geekymon2/carmarketplace/carinfoservice/data/ModelsData.java
@@ -1,15 +1,26 @@
 package com.geekymon2.carmarketplace.carinfoservice.data;
 
-import com.geekymon2.carmarketplace.carinfoservice.models.CarMake;
 import com.geekymon2.carmarketplace.carinfoservice.models.CarModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModelsData {
+
+    private ModelsData() {
+        super();
+    }
+
     public static List<CarModel> getModelsData(String makeName) {
-        List<CarModel> models = new ArrayList<CarModel>();
-        models.add(new CarModel("FALCON"));
+
+        List<CarModel> models = new ArrayList<>();
+        if (makeName.equals("FORD")) {
+            models.add(new CarModel("FALCON"));
+        }
+        else {
+            models.add(new CarModel("TUCSON"));
+        }
+
         return models;
     }
 }
