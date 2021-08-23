927b05e6d8a76e8c15976a6b8daebf4820584438
Geeky Mon2 <geekymon2@gmail.com>
2021-08-23 21:43:54 +1000

    trying lombok to reduce boilerplate

--- car-info-service/src/main/java/com/geekymon2/carmarketplace/carinfoservice/entities/CarMake.java
+++ car-info-service/src/main/java/com/geekymon2/carmarketplace/carinfoservice/entities/CarMake.java
@@ -1,6 +1,8 @@
 package com.geekymon2.carmarketplace.carinfoservice.entities;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -10,6 +12,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity(name = "make")
 public class CarMake {
 
@@ -23,58 +28,10 @@ public class CarMake {
     @OneToMany(mappedBy = "make")
     private Set<CarModel> models;
 
-    public CarMake() {
-    }
 
     public CarMake(Long id, String name, String country) {
         this.id = id;
         this.name = name;
         this.country = country;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Set<CarModel> getModels() {
-        return models;
-    }
-
-    public void setModels(Set<CarModel> models) {
-        this.models = models;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CarMake carMake = (CarMake) o;
-        return id.equals(carMake.id) && name.equals(carMake.name) && country.equals(carMake.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, country);
     }
 }
