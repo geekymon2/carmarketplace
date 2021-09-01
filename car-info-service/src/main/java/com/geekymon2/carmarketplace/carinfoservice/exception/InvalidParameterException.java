33a491626696aa56ac34af946c4e0d0820c084cf
Geeky Mon2 <geekymon2@gmail.com>
2021-09-01 17:58:21 +1000

    refactor, moved services to independent repos.

--- car-info-service/src/main/java/com/geekymon2/carmarketplace/carinfoservice/exception/InvalidParameterException.java
+++
@@ -1,9 +0,0 @@
-package com.geekymon2.carmarketplace.carinfoservice.exception;
-
-public class InvalidParameterException  extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidParameterException(String message) {
-        super(message);
-    }
-}
--- car-info-service/src/main/java/com/geekymon2/carmarketplace/carinfoservice/repository/CarMakeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.geekymon2.carmarketplace.carinfoservice.repository;
-
-import com.geekymon2.carmarketplace.carinfoservice.entities.CarMake;
-
-public interface CarMakeRepository extends ReadOnlyRepository<CarMake, Long> {
-}
