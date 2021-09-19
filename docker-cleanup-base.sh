f5f3186ef5846ed4c8c0c479e2386ac807a1e166
Geeky Mon2 <geekymon2@gmail.com>
2021-09-19 15:04:43 +1000

    wordings

---
+++ docker-cleanup-base.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "Running environment cleanup for base images on $HOSTNAME"
+# Stop and remove all base containers
+docker-compose -f -s rm
--- docker-cleanup-root.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "Running environment cleanup for root images on $HOSTNAME"
-# Stop and remove all root containers
-docker-compose -f -s rm
