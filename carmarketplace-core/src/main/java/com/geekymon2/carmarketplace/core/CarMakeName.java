57a4e6130dd06fd8b926a8cdbe3d9c2e4adc8397
Geeky Mon2 <geekymon2@gmail.com>
2021-10-09 22:20:01 +1100

    added core for core libraries

---
+++ carmarketplace-core/src/main/java/com/geekymon2/carmarketplace/core/CarMakeName.java
@@ -0,0 +1,24 @@
+package com.geekymon2.carmarketplace.core;
+
+public enum CarMakeName {
+    FORD,
+    AUDI,
+    BMW,
+    MAZDA,
+    TOYOTA,
+    FERRARI,
+    HOLDEN,
+    HONDA,
+    HYUNDIA,
+    KIA,
+    LAMBORGHINI,
+    LANDROVER,
+    LEXUS,
+    MERCEDESBENZ,
+    MITSUBISHI,
+    NISSAN,
+    SUBARU,
+    SUZUKI,
+    VOLKSWAGEN,
+    VOLVO
+}
