e95a36324d086857b48d0ce94a9b36cc52721916
Geeky Mon2 <geekymon2@gmail.com>
2022-07-02 18:06:37 +1000

    modified run/debug config

--- carmarketplace-core/src/main/java/com/geekymon2/carmarketplace/core/security/autoconfiguration/properties/ApiConfig.java
+++ carmarketplace-core/src/main/java/com/geekymon2/carmarketplace/core/security/autoconfiguration/properties/ApiConfig.java
@@ -2,11 +2,13 @@ package com.geekymon2.carmarketplace.core.security.autoconfiguration.properties;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
 @Configuration
+@ConfigurationProperties
 public class ApiConfig {
     private SecurityConfig securityConfig;
 }
