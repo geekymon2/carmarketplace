d8ed685f9e0b57160b88308184fecb1a7146bee8
geekymon2 <geekymon2@gmail.com>
2024-04-29 18:34:48 +1000

    Update JwtConfig.java

--- carmarketplace-core/src/main/java/com/geekymon2/carmarketplace/core/autoconfiguration/security/properties/JwtConfig.java
+++ carmarketplace-core/src/main/java/com/geekymon2/carmarketplace/core/autoconfiguration/security/properties/JwtConfig.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Getter
 @Setter
 @Configuration
+@Primary
 @ConfigurationProperties(prefix = "jwt-config")
 public class JwtConfig {
     private Boolean jwtDisabled;
