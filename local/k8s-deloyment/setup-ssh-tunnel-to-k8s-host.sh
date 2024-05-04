b7ed7f4563ea8a6ff1674542dc9ef5e927390e37
geekymon2 <geekymon2@gmail.com>
2024-05-04 21:34:17 +1000

    update docs

--- local/k8s-deloyment/setup-ssh-tunnel-to-k8s-host.sh
+++ local/k8s-deloyment/setup-ssh-tunnel-to-k8s-host.sh
@@ -1,4 +1,9 @@
 #!bin/bash
-# README: This script
+# This script enables ssh tunnel between workstation and k8s server to allow
+# access to minikube dashboard on the workstation to manage the cluster
+# on the k8s server minikube is running and using proxy with port 8001
+# on the local workstation port is 8181
+# Access to dashboard locally via the url below.
+####: http://localhost:8181/api/v1/namespaces/kubernetes-dashboard/services/http:kubernetes-dashboard:/proxy/
 
-ssh -f -N mon2@mon2desktop -L 8181:localhost:8001
+ssh -L 8181:localhost:8001 mon2@mon2desktop
--- readme/LINUXHELP.md
+++ readme/LINUXHELP.md
@@ -2,3 +2,10 @@
 
 ### Check Port Usage
 - $sudo lsof -i -P -n | grep LISTEN
+
+### SystemD Service Commands
+- systemctl status {service-name}
+- systemctl list-units --type=service
+- systemctl stop {service-name}
+- systemctl start {service-name}
+
