94ea6a94ced917c4c0a63d363c42c8eedd0aeb1d
geekymon2 <geekymon2@gmail.com>
2024-05-09 14:00:42 +1000

    added more definitions

--- env-setup/k8s-deloyment/create_secrets.sh
+++ env-setup/k8s-deloyment/create_secrets.sh
@@ -1,3 +1,4 @@
 
 kubectl create secret generic encryptkeysecret --namespace carmarketplace-dev --from-literal encryptkeysecret=<value>
 kubectl create secret generic carinfodbpassword --namespace carmarketplace-dev --from-literal carinfodbpassword=<value>
+kubectl create secret generic carlistingdbpassword --namespace carmarketplace-dev --from-literal carlistingdbpassword=<value>
