cdeb9383dc1b30649fad2eace5696d9cd6122286
Geeky Mon2 <geekymon2@gmail.com>
2024-05-11 15:40:17 +1000

    Update init.sql

--- env-setup/db-init/init.sql
+++ env-setup/db-init/init.sql
@@ -6,11 +6,12 @@ FLUSH PRIVILEGES;
 -- create schemas for local development
 CREATE DATABASE IF NOT EXISTS carinfodbdev
 CREATE DATABASE IF NOT EXISTS carlistingdbdev
+CREATE DATABASE IF NOT EXISTS userdbdev
 
 -- create schemas for ci environment
 CREATE DATABASE IF NOT EXISTS carinfodbci
 CREATE DATABASE IF NOT EXISTS carlistingdbci
-
+CREATE DATABASE IF NOT EXISTS userdbci
 -- create tables
 
 -- create data
