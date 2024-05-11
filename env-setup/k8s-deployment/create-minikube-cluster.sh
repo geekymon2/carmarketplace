5d70c85a311c2264c326aa4fa0edc43ef56353e2
Geeky Mon2 <geekymon2@gmail.com>
2024-05-11 16:57:38 +1000

    update

--- env-setup/k8s-deployment/create-minikube-cluster.sh
+++ env-setup/k8s-deployment/create-minikube-cluster.sh
@@ -4,5 +4,5 @@ minikube addons enable metrics-server
 minikube addons enable dashboard
 minikube addons enable ingress
 minikube addons enable ingress-dns
-kubectl proxy --address 0.0.0.0 --disable-filter=true &
+kubectl proxy --address 0.0.0.0 --disable-filter=true &&
 minikube dashboard --url
--- env-setup/k8s-deployment/create-secrets.sh
+++ env-setup/k8s-deployment/create-secrets.sh
@@ -2,3 +2,4 @@ kubectl create secret generic encryptkeysecret --namespace carmarketplace-dev --
 kubectl create secret generic carinfodbpassword --namespace carmarketplace-dev --from-literal carinfodbpassword=$CARINFO_DB_PASSWORD
 kubectl create secret generic carlistingdbpassword --namespace carmarketplace-dev --from-literal carlistingdbpassword=$CARLISTING_DB_PASSWORD
 kubectl create secret generic userdbpassword --namespace carmarketplace-dev --from-literal userdbpassword=$USER_DB_PASSWORD
+kubectl create secret generic jwtauthsecret --namespace carmarketplace-dev --from-literal jwtauthsecret=$JWT_AUTH_SECRET
