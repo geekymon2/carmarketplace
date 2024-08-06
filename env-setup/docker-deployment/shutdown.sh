a4da308007dc639735f425f97acb8e90e744f3d7
Geeky Mon2 <geekymon2@gmail.com>
2024-08-06 16:33:04 +1000

    removed docker scripts

--- env-setup/docker-deployment/shutdown.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "Running environment cleanup for base images on $HOSTNAME"
-# Stop and remove all base containers
-docker-compose rm -f -s
--- env-setup/docker-deployment/startup.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "Running environment startup for base images on $HOSTNAME"
-# Start all base containers
-docker-compose up -d
