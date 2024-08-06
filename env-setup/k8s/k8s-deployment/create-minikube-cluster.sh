51811e1965403f527dac3b13c2592724bc975252
Geeky Mon2 <geekymon2@gmail.com>
2024-08-06 16:23:58 +1000

    moved environment setup into its own repo

--- env-setup/k8s/k8s-deployment/create-minikube-cluster.sh
+++
@@ -1,8 +0,0 @@
-minikube start --cpus=8 --memory=8192 --vm-driver=virtualbox
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-minikube addons enable metrics-server
-minikube addons enable dashboard
-minikube addons enable ingress
-minikube addons enable ingress-dns
-kubectl proxy --address 0.0.0.0 --disable-filter=true &&
-minikube dashboard --url
--- env-setup/k8s/k8s-deployment/create-secrets.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret generic encryptkeysecret --namespace carmarketplace-dev --from-literal encryptkeysecret=$ENCRYPT_KEY_SECRET
-kubectl create secret generic carinfodbpassword --namespace carmarketplace-dev --from-literal carinfodbpassword=$CARINFO_DB_PASSWORD
-kubectl create secret generic carlistingdbpassword --namespace carmarketplace-dev --from-literal carlistingdbpassword=$CARLISTING_DB_PASSWORD
-kubectl create secret generic userdbpassword --namespace carmarketplace-dev --from-literal userdbpassword=$USER_DB_PASSWORD
-kubectl create secret generic jwtauthsecret --namespace carmarketplace-dev --from-literal jwtauthsecret=$JWT_AUTH_SECRET
